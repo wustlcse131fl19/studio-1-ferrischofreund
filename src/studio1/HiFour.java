6d162a3a150507a7968cd772c97388d03d660333
hana6971 <cho.h@wustl.edu>
2019-08-29 12:07:23 -0500

    Jacob Freund Jack Ferris Jay Cho

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -9,13 +9,16 @@ import support.cse131.ArgsProcessor;
 public class HiFour {
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
-		String s0 = ap.nextString("Enter value for arg 0");
-		String s1 = ap.nextString("Enter value for arg 1");
-		String s2 = ap.nextString("Enter value for arg 2");
-		String s3 = ap.nextString("Enter value for arg 3");
+		String name1 = ap.nextString("What is the first group members name?");
+		String name2 = ap.nextString("Second group members name?");
+		String name3 = ap.nextString("Third group members name?");
+		String name4 = ap.nextString("Last group members name?");
 		//
 		// Say hello to the names in s0 through s3.
-		//
+		//ArgsProcessor ap = new ArgsProcessor(args);
+		//		String name = ap.nextString("What is your name?  ");
+		//		System.out.println("Hi " + name + ".  How are you?");
+		System.out.println("Greetings " + name1 + ", " + name2 + ", " + name3 + ", and " + name4 + ".");
 
 	}
 }
